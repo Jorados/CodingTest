8f6732a14e8ffec667eda82ab37693ba29a96b9f
Seongjin Jo <seongjin8860@naver.com>
2023-02-25 23:52:28 +0900

    baekjoon 문제

--- src/Baekjoon/ex3.java
+++ src/Baekjoon/ex3.java
@@ -3,6 +3,7 @@ package Baekjoon;
 import java.util.Scanner;
 import java.util.Stack;
 
+//백준 9935 문자열 폭발
 public class ex3 {
     public static String solution(String s,String boom){
         String answer="";
@@ -10,15 +11,29 @@ public class ex3 {
         StringBuilder sb = new StringBuilder();
         for(int i=0; i<s.length(); i++){
             stack.push(s.charAt(i));
-
             if(stack.size() >= boom.length()) {
+                boolean flag = true;
                 for(int j=0; j<boom.length(); j++){
-                    Character pop = stack.pop();
+                    if(stack.get(stack.size()-boom.length() + j) != boom.charAt(j)){
+                        flag = false;
+                        break;
+                    }
+                }
+
+                if(flag){
+                    for(int j=0; j<boom.length(); j++){
+                        stack.pop();
+                    }
                 }
             }
         }
 
+        for(char ch : stack){
+            sb.append(ch);
+        }
 
+        if(sb.length() > 0) answer=sb.toString();
+        else answer="FRULA";
         return answer;
     }
 
