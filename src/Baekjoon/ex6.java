276bf5f5a3db7b93a929d0d495846728878bd3b6
Seongjin Jo <seongjin8860@naver.com>
2023-03-01 15:54:25 +0900

    baekjoon 단지번호 붙이기

--- src/Baekjoon/ex6.java
+++ src/Baekjoon/ex6.java
@@ -1,13 +1,12 @@
 package Baekjoon;
-
-
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 //백준 2583 - 단지 번호 붙이기 S1
 public class ex6 {
 
-    static int n,answer=0,cnt=0;
+    static int n,cnt=1;
     static ArrayList<Integer> arr = new ArrayList<>();
     static int[][] board;
     static int[][] ch;
@@ -15,11 +14,11 @@ public class ex6 {
     static int[] dy={-1,0,1,0};
 
     public static void DFS(int x,int y,int[][] board){
+        ch[x][y]=1;
         for(int i=0; i<4; i++){
             int nx = x + dx[i];
             int ny = y + dy[i];
-            if(nx>=0 && nx<n && ny>=0 && ny<n && board[nx][ny]==1){
-                board[nx][ny]=0;
+            if(nx>=0 && nx<n && ny>=0 && ny<n && board[nx][ny]==1 && ch[nx][ny]!=1){
                 cnt++;
                 DFS(nx,ny,board);
             }
@@ -28,11 +27,10 @@ public class ex6 {
     public static void solution(int[][] board){
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
-                if(board[i][j]==1){
-                    answer++;
+                if(board[i][j]==1 && ch[i][j]==0){
                     DFS(i,j,board);
                     arr.add(cnt);
-                    cnt=0;
+                    cnt=1;
                 }
             }
         }
@@ -40,19 +38,22 @@ public class ex6 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         n=sc.nextInt();
         board = new int[n][n];
         ch = new int[n][n];
+
         for(int i=0; i<n; i++){
+            String input = sc.next();
             for(int j=0; j<n; j++){
-                board[i][j] = sc.nextInt();
+                board[i][j]=input.charAt(j)-'0';
+                //board[i][j]=Character.getNumericValue(input.charAt(j));
             }
         }
         solution(board);
         DFS(0,0,board);
 
-        System.out.println(answer);
+        Collections.sort(arr);
+        System.out.println(arr.size());
         for(int x : arr){
             System.out.println(x);
         }
