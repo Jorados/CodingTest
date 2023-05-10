bca1df3d02e2ca6222eac730e1d6602d5f92db7a
seongjin8860 <seongjin8860@naver.com>
2023-05-10 15:18:19 +0900

    백준 문제 풀이

---
+++ src/Baekjoon/ex11724.java
@@ -0,0 +1,52 @@
+package Baekjoon;
+
+
+import java.util.Scanner;
+
+// 연결 요소의 개수 - S2
+public class ex11724 {
+
+    static int n; // 정점의 개수
+    static int m; // 간선의 개수
+    static int[][] graph;
+    static boolean[] ch;
+    static int cnt=0;
+
+    public static void DFS(int v){
+        if(ch[v]==true) return;
+        else{
+            ch[v]=true;
+            for(int i=1; i<=n; i++){
+                if(graph[v][i]==1){
+                    DFS(i);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        graph = new int[n+1][n+1];
+        ch = new boolean[n+1];
+
+        for(int i=0; i<m; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            graph[a][b] = 1;
+            graph[b][a] = 1;
+        }
+
+        for(int i=1; i<=n; i++){
+            if(!ch[i]) {
+                DFS(i);
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+
+}
