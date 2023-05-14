96b194cd9f000cc6efbaf9d9cba8345b4141ceca
seongjin8860 <seongjin8860@naver.com>
2023-05-14 17:40:09 +0900

    백준 문제 풀이

--- src/Baekjoon/ex9461.java
+++ src/Baekjoon/ex9461.java
@@ -6,22 +6,29 @@ import java.util.Scanner;
 // 파도반 수열 - S3 - DP
 public class ex9461 {
     static int n;
-    static int[] arr;
-    static int[] dp;
+    static long[] dp;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         n = sc.nextInt();
-        dp = new int[100];
+
+        dp = new long[101];
 
         dp[0]=1;
         dp[1]=1;
         dp[2]=1;
+        dp[3]=1;
+        dp[4]=2;
+        dp[5]=2;
 
-        for(int i=3; i<dp.length; i++){
-            dp[i] = dp[i-2] + dp[i-3];
+        for(int i=6; i<dp.length; i++){
+            dp[i] = dp[i-1] + dp[i-5];
         }
-        System.out.println(dp[n-1]);
+
+        while(n-- >0){
+            int s = sc.nextInt();
+            System.out.println(dp[s]);
+        }
 
     }
 }
