7bfaa7ad835b952904edbf8dd8062c2b2ca27e2c
sss_jinn <100845256+Jorados@users.noreply.github.com>
2023-06-25 16:09:57 +0900

    백준 DFS 문제 풀이

---
+++ src/Baekjoon/ex22352.java
@@ -0,0 +1,69 @@
+package Baekjoon;
+
+
+import java.util.Scanner;
+
+// 항체 인식 - G5 - DFS
+public class ex22352 {
+
+    static int n,m;
+    static int[][] board;
+    static int[][] compare;
+    // 한 영역만 바뀌어 있으면 됨
+    // 위에 4*4까지 탐색후, 밑을 DFS()로 침투한 후에 같으면 0으로 만들어버리고 다르면 true?
+    static boolean flag=false;
+    static String answer="YES";
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int[] diffInfo = new int[4];
+
+
+    public static void DFS(int x,int y,int before, int after){
+        board[x][y]=after;
+        for(int i=0; i<4; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if(nx>=0 && nx<n && ny>=0 && ny<m && board[nx][ny]==before){
+                DFS(nx,ny,before,after);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        board = new int[n][m];
+        compare = new int[n][m];
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                board[i][j]=sc.nextInt();
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                compare[i][j] = sc.nextInt();
+                if(compare[i][j]!=board[i][j] && !flag){
+                    flag=true;
+                    diffInfo[0] = i;
+                    diffInfo[1] = j;
+                    diffInfo[2] = board[i][j];
+                    diffInfo[3] = compare[i][j];
+                }
+            }
+        }
+
+        if(flag) DFS(diffInfo[0],diffInfo[1],diffInfo[2],diffInfo[3]);
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(board[i][j]!=compare[i][j]) answer = "NO";
+            }
+        }
+        System.out.println(answer);
+    }
+}
