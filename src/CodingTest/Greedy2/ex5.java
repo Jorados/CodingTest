822466fdb23b3618519a179b0aa6cad16ba25b26
seongjin8860 <seongjin8860@naver.com>
2023-10-11 21:18:30 +0900

    최소스패닝트리

--- src/CodingTest/Greedy2/ex5.java
+++ src/CodingTest/Greedy2/ex5.java
@@ -1,10 +1,7 @@
 package CodingTest.Greedy2;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.Scanner;
+import java.util.*;
 
 // 다익스트라 알고리즘
 class Edge implements Comparable<Edge>{
@@ -14,6 +11,7 @@ class Edge implements Comparable<Edge>{
         this.vex = vex;
         this.cost = cost;
     }
+
     @Override
     public int compareTo(Edge o){
         return this.cost - o.cost;
@@ -28,8 +26,8 @@ public class ex5 {
     public static void BFS(int v){
         PriorityQueue<Edge> pq = new PriorityQueue<>();
         pq.offer(new Edge(v,0));
+        dis[v]=0;
 
-        dis[v] = 0;
         while (!pq.isEmpty()){
             Edge poll = pq.poll();
             int now = poll.vex;
@@ -38,25 +36,25 @@ public class ex5 {
             if(nowCost>dis[now]) continue;
 
             for(Edge x : graph.get(now)){
-                if(nowCost + x.cost < dis[x.vex]){
-                    dis[x.vex] = nowCost + x.cost;
-                    pq.offer(new Edge(x.vex,nowCost+x.cost));
+                if(x.cost + nowCost < dis[x.vex]){
+                    dis[x.vex] = x.cost + nowCost;
+                    pq.offer(new Edge(x.vex,nowCost + x.cost));
                 }
             }
         }
-
-
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
 
+        n = sc.nextInt();
         graph = new ArrayList<ArrayList<Edge>>();
         for(int i=0; i<=n; i++){
             graph.add(new ArrayList<>());
         }
+        dis = new int[n+1];
+        Arrays.fill(dis,Integer.MAX_VALUE);
 
+        m = sc.nextInt();
         for(int i=0; i<m; i++){
             int a = sc.nextInt();
             int b = sc.nextInt();
@@ -65,14 +63,10 @@ public class ex5 {
             graph.get(a).add(new Edge(b,c));
         }
 
-        dis = new int[n+1];
-        Arrays.fill(dis,Integer.MAX_VALUE);
-
         BFS(1);
-
         for(int i=2; i<=n; i++){
-            if(dis[i]!=Integer.MAX_VALUE) System.out.println(i+ " : " + dis[i] + " ");
-            else System.out.println(i + " : " + "impossible");
+            if(dis[i] != Integer.MAX_VALUE) System.out.println("~~");
+            else System.out.println("~~");
         }
     }
 }
---
+++ src/CodingTest/Greedy2/ex8.java
@@ -0,0 +1,72 @@
+package CodingTest.Greedy2;
+
+
+// 원더랜드 (최소스패닝 트리) --> 최소비용으로 그래프를 연결하는거임.
+
+
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+class Dot implements Comparable<Dot>{
+    int vex;
+    int cost;
+    public Dot(int vex,int cost){
+        this.vex = vex;
+        this.cost = cost;
+    }
+
+    @Override
+    public int compareTo(Dot o){
+        return this.cost - o.cost;
+    }
+}
+public class ex8 {
+    static int n,m;
+    static int answer=0;
+    static boolean[] ch;
+    static ArrayList<ArrayList<Dot>> graph;
+
+    public static void BFS(int vex){
+        PriorityQueue<Dot> pq = new PriorityQueue<>();
+        pq.offer(new Dot(vex,0));
+
+        while (!pq.isEmpty()){
+            Dot tmp = pq.poll(); // 여기서 뺄때 정렬된채로 나온다.
+            int v = tmp.vex;
+
+            if(!ch[v]){
+                ch[v]=true;
+                answer+=tmp.cost;
+                for(Dot x : graph.get(v)){
+                    if(!ch[x.vex]) {
+                        pq.offer(new Dot(x.vex,x.cost));
+                    }
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        graph = new ArrayList<ArrayList<Dot>>();
+        for(int i=0; i<=n; i++){
+            graph.add(new ArrayList<>());
+        }
+
+        m = sc.nextInt();
+        for(int i=0; i<m; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+
+            graph.get(a).add(new Dot(b,c));
+            graph.get(b).add(new Dot(a,c));
+        }
+
+        ch = new boolean[n+1];
+        BFS(1);
+        System.out.println(answer);
+    }
+}
